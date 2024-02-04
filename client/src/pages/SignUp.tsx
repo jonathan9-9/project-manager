d89e30473e7b57ff5ffd5a194af50b3ddc226614
Jonathan <jonathancornejox@gmail.com>
2024-02-04 15:57:41 -0800

    form input fields and button ui

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Center,
   FormControl,
   FormErrorMessage,
@@ -42,40 +43,84 @@ const SignUp = () => {
         </Text>
       </Center>
       <Center>
-        <Box
-          w="45%"
-          alignItems="center"
-          display="flex"
-          flexDirection="column"
-          p="4"
-          boxShadow="0 4px 8px rgba(0, 0, 0, 0.1)"
-        >
-          <FormControl isInvalid={isErrorName} isRequired>
-            <FormLabel>Name</FormLabel>
-            <Input
-              type="text"
-              value={nameInput}
-              onChange={handleNameInputChange}
-            />
-            {!isErrorName ? (
-              <FormHelperText>Enter legal name.</FormHelperText>
-            ) : (
-              <FormErrorMessage>Name is required.</FormErrorMessage>
-            )}
-          </FormControl>
-          <FormControl isInvalid={isErrorEmail} isRequired>
-            <FormLabel>Email</FormLabel>
-            <Input
-              type="email"
-              value={emailInput}
-              onChange={handleEmailInputChange}
-            />
-            {!isErrorEmail ? (
-              <FormHelperText>Enter valid email.</FormHelperText>
-            ) : (
-              <FormErrorMessage>Email is required.</FormErrorMessage>
-            )}
-          </FormControl>
+        <Box w="42%" p="4" boxShadow="0 4px 8px rgba(0, 0, 0, 0.1)">
+          <form>
+            <FormControl isInvalid={isErrorName} isRequired>
+              <FormLabel>Name</FormLabel>
+              <Input
+                id="name"
+                type="text"
+                value={nameInput}
+                onChange={handleNameInputChange}
+              />
+              {!isErrorName ? (
+                <FormHelperText>Enter legal name.</FormHelperText>
+              ) : (
+                <FormErrorMessage>Name is required.</FormErrorMessage>
+              )}
+            </FormControl>
+            <FormControl isInvalid={isErrorEmail} isRequired>
+              <FormLabel>Email</FormLabel>
+              <Input
+                type="email"
+                value={emailInput}
+                onChange={handleEmailInputChange}
+              />
+              {!isErrorEmail ? (
+                <FormHelperText>Enter valid email.</FormHelperText>
+              ) : (
+                <FormErrorMessage>Email is required.</FormErrorMessage>
+              )}
+            </FormControl>
+            <FormControl isInvalid={isErrorUsername} isRequired>
+              <FormLabel>Username</FormLabel>
+              <Input
+                type="text"
+                value={usernameInput}
+                onChange={handleUsernameInputChange}
+              />
+              {!isErrorEmail ? (
+                <FormHelperText>Enter username.</FormHelperText>
+              ) : (
+                <FormErrorMessage>Username is required.</FormErrorMessage>
+              )}
+            </FormControl>
+            <FormControl isInvalid={isErrorPhoto} isRequired>
+              <FormLabel>Photo</FormLabel>
+              <Input
+                type="text"
+                value={photoInput}
+                onChange={handlePhotoInputChange}
+              />
+              {!isErrorPhoto ? (
+                <FormHelperText>Add your profile photo.</FormHelperText>
+              ) : (
+                <FormErrorMessage>Profile photo is required.</FormErrorMessage>
+              )}
+            </FormControl>
+            <FormControl isInvalid={isErrorPassword} isRequired mb={7}>
+              <FormLabel>Password</FormLabel>
+              <Input
+                type="password"
+                value={passwordInput}
+                onChange={handlePasswordInputChange}
+              />
+              {!isErrorPassword ? (
+                <FormHelperText>Enter your password</FormHelperText>
+              ) : (
+                <FormErrorMessage>Password is required.</FormErrorMessage>
+              )}
+            </FormControl>
+            <Button
+              style={{ backgroundColor: "#3498db", color: "#FFFFFF" }}
+              size="md"
+              height="42px"
+              width="150px"
+              p={3}
+            >
+              Create account
+            </Button>
+          </form>
         </Box>
       </Center>
     </>
