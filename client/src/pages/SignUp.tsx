24d8064b2922e62c0a876fad44af0ea009857572
Jonathan <jonathancornejox@gmail.com>
2024-02-04 19:17:17 -0800

    sign up form error message and logic form submission setup

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -18,22 +18,73 @@ const SignUp = () => {
   const [photoInput, setPhotoInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
 
-  const isErrorName = nameInput === "";
-  const isErrorEmail = emailInput === "";
-  const isErrorUsername = usernameInput === "";
-  const isErrorPhoto = photoInput === "";
-  const isErrorPassword = passwordInput === "";
+  const [clickedSubmitName, setClickedSubmitName] = useState(false);
+  const [clickedSubmitEmail, setClickedSubmitEmail] = useState(false);
+  const [clickedSubmitUsername, setClickedSubmitUsername] = useState(false);
+  const [clickedSubmitPhoto, setClickedSubmitPhoto] = useState(false);
+  const [clickedSubmitPassword, setClickedSubmitPassword] = useState(false);
 
-  const handleNameInputChange = (e: React.FormEvent<HTMLInputElement>) =>
+  const isErrorName = nameInput === "" && clickedSubmitName;
+  const isErrorEmail = emailInput === "" && clickedSubmitEmail;
+  const isErrorUsername = usernameInput === "" && clickedSubmitUsername;
+  const isErrorPhoto = photoInput === "" && clickedSubmitPhoto;
+  const isErrorPassword = passwordInput === "" && clickedSubmitPassword;
+
+  const handleNameInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setClickedSubmitName(true);
     setNameInput((e.target as HTMLInputElement).value);
-  const handleEmailInputChange = (e: React.FormEvent<HTMLInputElement>) =>
+  };
+  const handleEmailInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setClickedSubmitEmail(true);
     setEmailInput((e.target as HTMLInputElement).value);
-  const handleUsernameInputChange = (e: React.FormEvent<HTMLInputElement>) =>
+  };
+  const handleUsernameInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setClickedSubmitUsername(true);
     setUsernameInput((e.target as HTMLInputElement).value);
-  const handlePhotoInputChange = (e: React.FormEvent<HTMLInputElement>) =>
+  };
+  const handlePhotoInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setClickedSubmitPhoto(true);
     setPhotoInput((e.target as HTMLInputElement).value);
-  const handlePasswordInputChange = (e: React.FormEvent<HTMLInputElement>) =>
+  };
+  const handlePasswordInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setClickedSubmitPassword(true);
     setPasswordInput((e.target as HTMLInputElement).value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Submit button clicked");
+
+    setClickedSubmitName(true);
+    setClickedSubmitEmail(true);
+    setClickedSubmitUsername(true);
+    setClickedSubmitPhoto(true);
+    setClickedSubmitPassword(true);
+
+    if (
+      nameInput === "" ||
+      emailInput === "" ||
+      usernameInput === "" ||
+      photoInput === "" ||
+      passwordInput === ""
+    ) {
+      console.log("All fields are required");
+    } else {
+      console.log("form submit successfully");
+
+      setNameInput("");
+      setEmailInput("");
+      setUsernameInput("");
+      setPhotoInput("");
+      setPasswordInput("");
+
+      setClickedSubmitName(false);
+      setClickedSubmitEmail(false);
+      setClickedSubmitUsername(false);
+      setClickedSubmitPhoto(false);
+      setClickedSubmitPassword(false);
+    }
+  };
 
   return (
     <>
@@ -44,83 +95,81 @@ const SignUp = () => {
       </Center>
       <Center>
         <Box w="42%" p="4" boxShadow="0 4px 8px rgba(0, 0, 0, 0.1)">
-          <form>
-            <FormControl isInvalid={isErrorName} isRequired>
-              <FormLabel>Name</FormLabel>
-              <Input
-                id="name"
-                type="text"
-                value={nameInput}
-                onChange={handleNameInputChange}
-              />
-              {!isErrorName ? (
-                <FormHelperText>Enter legal name.</FormHelperText>
-              ) : (
-                <FormErrorMessage>Name is required.</FormErrorMessage>
-              )}
-            </FormControl>
-            <FormControl isInvalid={isErrorEmail} isRequired>
-              <FormLabel>Email</FormLabel>
-              <Input
-                type="email"
-                value={emailInput}
-                onChange={handleEmailInputChange}
-              />
-              {!isErrorEmail ? (
-                <FormHelperText>Enter valid email.</FormHelperText>
-              ) : (
-                <FormErrorMessage>Email is required.</FormErrorMessage>
-              )}
-            </FormControl>
-            <FormControl isInvalid={isErrorUsername} isRequired>
-              <FormLabel>Username</FormLabel>
-              <Input
-                type="text"
-                value={usernameInput}
-                onChange={handleUsernameInputChange}
-              />
-              {!isErrorEmail ? (
-                <FormHelperText>Enter username.</FormHelperText>
-              ) : (
-                <FormErrorMessage>Username is required.</FormErrorMessage>
-              )}
-            </FormControl>
-            <FormControl isInvalid={isErrorPhoto} isRequired>
-              <FormLabel>Photo</FormLabel>
-              <Input
-                type="text"
-                value={photoInput}
-                onChange={handlePhotoInputChange}
-              />
-              {!isErrorPhoto ? (
-                <FormHelperText>Add your profile photo.</FormHelperText>
-              ) : (
-                <FormErrorMessage>Profile photo is required.</FormErrorMessage>
-              )}
-            </FormControl>
-            <FormControl isInvalid={isErrorPassword} isRequired mb={7}>
-              <FormLabel>Password</FormLabel>
-              <Input
-                type="password"
-                value={passwordInput}
-                onChange={handlePasswordInputChange}
-              />
-              {!isErrorPassword ? (
-                <FormHelperText>Enter your password</FormHelperText>
-              ) : (
-                <FormErrorMessage>Password is required.</FormErrorMessage>
-              )}
-            </FormControl>
-            <Button
-              style={{ backgroundColor: "#3498db", color: "#FFFFFF" }}
-              size="md"
-              height="42px"
-              width="150px"
-              p={3}
-            >
-              Create account
-            </Button>
-          </form>
+          <FormControl isInvalid={isErrorName} isRequired>
+            <FormLabel>Name</FormLabel>
+            <Input
+              id="name"
+              type="text"
+              value={nameInput}
+              onChange={handleNameInputChange}
+            />
+            {!isErrorName ? null : (
+              <FormErrorMessage>Name is required.</FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl isInvalid={isErrorEmail} isRequired>
+            <FormLabel>Email</FormLabel>
+            <Input
+              type="email"
+              value={emailInput}
+              onChange={handleEmailInputChange}
+            />
+            {!isErrorEmail ? (
+              <FormHelperText>Enter valid email.</FormHelperText>
+            ) : (
+              <FormErrorMessage>Email is required.</FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl isInvalid={isErrorUsername} isRequired>
+            <FormLabel>Username</FormLabel>
+            <Input
+              type="text"
+              value={usernameInput}
+              onChange={handleUsernameInputChange}
+            />
+            {!isErrorUsername ? (
+              <FormHelperText>Enter username.</FormHelperText>
+            ) : (
+              <FormErrorMessage>Username is required.</FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl isInvalid={isErrorPhoto} isRequired>
+            <FormLabel>Photo</FormLabel>
+            <Input
+              type="text"
+              value={photoInput}
+              onChange={handlePhotoInputChange}
+            />
+            {!isErrorPhoto ? (
+              <FormHelperText>Add your profile photo.</FormHelperText>
+            ) : (
+              <FormErrorMessage>Profile photo is required.</FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl isInvalid={isErrorPassword} isRequired mb={7}>
+            <FormLabel>Password</FormLabel>
+            <Input
+              type="password"
+              value={passwordInput}
+              onChange={handlePasswordInputChange}
+            />
+            {!isErrorPassword ? (
+              <FormHelperText>Enter your password</FormHelperText>
+            ) : (
+              <FormErrorMessage>Password is required.</FormErrorMessage>
+            )}
+          </FormControl>
+          <Button
+            style={{ backgroundColor: "#3498db", color: "#FFFFFF" }}
+            size="md"
+            height="42px"
+            width="150px"
+            p={3}
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Create account
+          </Button>
         </Box>
       </Center>
     </>
