753fcb756e2b5c652b6345747dcbe5a43d028c37
Jonathan <jonathancornejox@gmail.com>
2024-02-16 17:01:34 -0800

    header nav links

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,18 +1,32 @@
 import { Box, Heading, Image } from "@chakra-ui/react";
 
+const components = [
+  { name: "Log in", path: "/login" },
+  { name: "Create an Account", path: "/sign-up" },
+  { name: "Projects", path: "/projects" },
+  { name: "Account Details", path: "/profile" },
+];
+
 const Header = () => {
   return (
-    <Box p={1} display="flex" border="1px solid black">
-      <Image
-        src="https://firebasestorage.googleapis.com/v0/b/my-first-project-portfol-6847b.appspot.com/o/vecteezy_suss-hundchen-hund-nein-hintergrund-logo-anwendbar-zu_27291212.png?alt=media&token=0df4b086-b59d-4b43-849b-381710d28236"
-        // fallbackSrc="https://via.placeholder.com/150"
-        alt="puppy logo"
-        borderRadius="50%"
-        boxSize="90px"
-      />
-      <Heading fontSize="30px" mt={5} mx={2}>
-        Project Management Tool
-      </Heading>
+    <Box display="flex" p={4} alignItems="center">
+      <Box p={1} display="flex" alignItems="center">
+        <Image
+          src="https://firebasestorage.googleapis.com/v0/b/my-first-project-portfol-6847b.appspot.com/o/vecteezy_suss-hundchen-hund-nein-hintergrund-logo-anwendbar-zu_27291212.png?alt=media&token=0df4b086-b59d-4b43-849b-381710d28236"
+          // fallbackSrc="https://via.placeholder.com/150"
+          alt="puppy logo"
+          borderRadius="50%"
+          boxSize="90px"
+        />
+        <Heading fontSize="26px" mt={5} mx={2}>
+          Project Management Tool
+        </Heading>
+      </Box>
+      <Box display="flex" justifyContent="space-around" w="70%">
+        {components.map((component) => {
+          return <Box>{component.name}</Box>;
+        })}
+      </Box>
     </Box>
   );
 };
