0139ab93609f1cbffd7561435fad9835d3933ecd
Jonathan <jonathancornejox@gmail.com>
2024-02-16 23:14:37 -0800

    react router navlink & favicon raven & css hover effect

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Box, Heading, Image } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
 
 const components = [
   { name: "Log in", path: "/login" },
@@ -12,11 +13,11 @@ const Header = () => {
     <Box display="flex" p={4} alignItems="center">
       <Box p={1} display="flex" alignItems="center">
         <Image
-          src="https://firebasestorage.googleapis.com/v0/b/my-first-project-portfol-6847b.appspot.com/o/vecteezy_suss-hundchen-hund-nein-hintergrund-logo-anwendbar-zu_27291212.png?alt=media&token=0df4b086-b59d-4b43-849b-381710d28236"
-          // fallbackSrc="https://via.placeholder.com/150"
+          src="https://firebasestorage.googleapis.com/v0/b/my-first-project-portfol-6847b.appspot.com/o/vecteezy_watercolor-paintings-of-a-black-crow-bird-ai-generated_33053087.png?alt=media&token=a5b0f204-b95b-44e3-8db5-fbfef6a4719c"
+          fallbackSrc="https://via.placeholder.com/150"
           alt="puppy logo"
           borderRadius="50%"
-          boxSize="90px"
+          boxSize="100px"
         />
         <Heading fontSize="26px" mt={5} mx={2}>
           Project Management Tool
@@ -24,7 +25,28 @@ const Header = () => {
       </Box>
       <Box display="flex" justifyContent="space-around" w="70%">
         {components.map((component) => {
-          return <Box>{component.name}</Box>;
+          return (
+            <NavLink
+              to={component.path}
+              style={({ isActive, isPending, isTransitioning }) => {
+                return {
+                  fontWeight: isActive ? "bold" : "",
+                  color: isPending ? "red" : "black",
+                };
+              }}
+            >
+              <Box
+                borderBottom="2px"
+                borderColor="transparent"
+                _hover={{
+                  borderColor: "#0077B5",
+                  transition: "border-color 0.5s ease-in-out",
+                }}
+              >
+                {component.name}
+              </Box>
+            </NavLink>
+          );
         })}
       </Box>
     </Box>
