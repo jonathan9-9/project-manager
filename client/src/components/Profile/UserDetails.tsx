ba3ea5448789df7ff18a128ac7656ac570e05093
Jonathan <jonathancornejox@gmail.com>
2024-02-19 12:01:26 -0800

    edit on press enter and save state values

--- client/src/components/Profile/UserDetails.tsx
+++ client/src/components/Profile/UserDetails.tsx
@@ -1,6 +1,8 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Input, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 import { FaUserEdit } from "react-icons/fa";
+import { GiCheckMark } from "react-icons/gi";
 
 interface Props {
   field: string;
@@ -8,16 +10,50 @@ interface Props {
 }
 
 const UserDetails = ({ field, value }: Props) => {
+  const [valueState, setValueState] = useState(value);
+  const [editField, setEditField] = useState(false);
+
+  const handleEditClick = () => {
+    setEditField(!editField);
+  };
+
+  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValueState(e.target.value);
+  };
+  const onPressEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      if (valueState.trim() === "") {
+        console.error("Input cannot be empty. Please enter a value.");
+      } else {
+        setEditField(false);
+      }
+    }
+  };
+
   return (
     <Flex>
       <Text lineHeight="32px" flex={1}>
         {field}
       </Text>
-      <Text lineHeight="32px" flex={1}>
-        {value}
-      </Text>
-      <Box>
-        <FaUserEdit cursor="pointer" onClick={() => {}} />
+      {editField ? (
+        <Input
+          value={valueState}
+          onChange={handleValueChange}
+          onKeyDown={(e) => onPressEnter(e)}
+          flex={1}
+          height="28px"
+        />
+      ) : (
+        <Text lineHeight="32px" flex={1}>
+          {valueState}
+        </Text>
+      )}
+      <Box ml={8}>
+        {editField ? (
+          <GiCheckMark cursor="pointer" onClick={handleEditClick} />
+        ) : (
+          <FaUserEdit cursor="pointer" onClick={handleEditClick} />
+        )}
       </Box>
     </Flex>
   );
