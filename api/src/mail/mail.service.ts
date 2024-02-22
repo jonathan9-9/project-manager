3ec9db5959c73ccbb41d2774d80cb4a3e3ba08c8
Jonathan <jonathancornejox@gmail.com>
2024-02-21 16:50:31 -0800

    installed nodemailer and its types and create transporter object

--- api/src/mail/mail.service.ts
+++ api/src/mail/mail.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { User as UserModel } from '@prisma/client';
+import { sendEmail } from './mail';
 
 @Injectable()
 export class MailService {
   async sendPasswordResetEmail(user: UserModel, token: string) {
-    console.log('User', user);
-    console.log('token', token);
-
-    return 'Hello';
+    console.log('TOKEN', token);
+    sendEmail(
+      {
+        from: 'jonathan3060@gmail.com',
+        to: user.email,
+        subject: 'Rest your password',
+        text: 'This is a test email sent using Nodemailer.',
+      },
+      () => {
+        console.log('Email sent for password reset');
+      },
+    );
   }
 }
---
+++ api/src/mail/mail.ts
@@ -0,0 +1,26 @@
+import * as nodemailer from 'nodemailer';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'jonathan3060@gmail.com',
+    pass: process.env.GMAIL_PASSWORD,
+  },
+});
+
+export const sendEmail = async (mailOptions, cb) => {
+  try {
+    const details = await transporter.sendMail(mailOptions);
+    cb(details);
+  } catch (err) {
+    console.log(err);
+  }
+};
