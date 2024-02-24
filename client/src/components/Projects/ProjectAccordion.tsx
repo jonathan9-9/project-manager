ed419356e01741f0647b0463c2b88591fb1f30e7
Jonathan <jonathancornejox@gmail.com>
2024-02-24 13:00:40 -0800

    accordion item open/close state

--- client/src/components/Projects/ProjectAccordion.tsx
+++ client/src/components/Projects/ProjectAccordion.tsx
@@ -14,28 +14,66 @@ import {
 
 import { AddIcon, CloseIcon } from "@chakra-ui/icons";
 import { useState } from "react";
+// import { UserProfileData } from "../../pages/Profile";
+import { Project } from "../../pages/Projects";
 
-const ProjectAccordion = () => {
+interface Props {
+  projects: Project[];
+  setProjects: React.Dispatch<React.SetStateAction<Project[]>>;
+}
+
+const ProjectAccordion = ({ projects, setProjects }: Props) => {
   const [name, setName] = useState("");
-  const [submittedName, setSubmittedName] = useState(false);
-
   const [description, setDescription] = useState("");
 
+  const [submittedName, setSubmittedName] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
   const onChangeName = (e: React.FormEvent) => {
+    setSubmittedName(false);
     setName((e.target as HTMLInputElement).value);
   };
   const onChangeDescription = (e: React.FormEvent) => {
     setDescription((e.target as HTMLInputElement).value);
   };
 
+  const onSubmitProjectCreation = () => {
+    setSubmittedName(true);
+
+    setIsOpen(false);
+
+    setProjects([
+      ...projects,
+      {
+        name,
+        description,
+        status: "Done",
+      },
+    ]);
+
+    setName("");
+    setDescription("");
+
+    setSubmittedName(false);
+
+    console.log("NAME", name);
+    console.log("DESCRIPTION:", description);
+  };
+
   const isErrorName = name === "" && submittedName;
   return (
-    <Accordion allowMultiple>
+    <Accordion allowToggle index={isOpen ? 0 : 1}>
       <AccordionItem>
         {({ isExpanded }) => (
           <>
             <h2>
-              <AccordionButton border="1px solid black" ml={2} mt={2} p={3}>
+              <AccordionButton
+                border="1px solid black"
+                ml={2}
+                mt={2}
+                p={3}
+                onClick={() => setIsOpen(!!!isOpen)}
+              >
                 {isExpanded ? (
                   <CloseIcon fontSize="10px" />
                 ) : (
@@ -47,16 +85,16 @@ const ProjectAccordion = () => {
               </AccordionButton>
             </h2>
             <AccordionPanel pb={2} flex="1" border="1px solid black" ml={2}>
-              <FormControl isInvalid={isErrorName} isRequired>
+              <FormControl isInvalid={isErrorName} mb={4} isRequired>
                 <FormLabel>Project Name:</FormLabel>
                 <Input
-                  id="password"
-                  type="password"
+                  id="name"
+                  type="text"
                   value={name}
                   onChange={onChangeName}
                 />
                 {!isErrorName ? null : (
-                  <FormErrorMessage>Password is required.</FormErrorMessage>
+                  <FormErrorMessage>Project name is required.</FormErrorMessage>
                 )}
               </FormControl>
 
@@ -64,7 +102,7 @@ const ProjectAccordion = () => {
                 <FormLabel>Description: </FormLabel>
                 <Textarea value={description} onChange={onChangeDescription} />
               </FormControl>
-              <Button w="100%" onClick={() => {}}>
+              <Button mt={4} w="100%" onClick={onSubmitProjectCreation}>
                 Create
               </Button>
             </AccordionPanel>
--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -5,55 +5,59 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ProjectAccordion from "../components/Projects/ProjectAccordion";
+import { useState } from "react";
 
-interface Project {
+export interface Project {
   name: string;
   description: string;
   status: string;
 }
 
+const mock_projects_data: Project[] = [
+  {
+    name: "Project A",
+    description:
+      "This is project A. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    status: "To Do",
+  },
+  {
+    name: "Project B",
+    description:
+      "This is project B. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    status: "In Progress",
+  },
+  {
+    name: "Project C",
+    description:
+      "This is project C. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    status: "In Progress",
+  },
+  {
+    name: "Project D",
+    description:
+      "This is project C. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    status: "Testing",
+  },
+  {
+    name: "Project E",
+    description:
+      "This is project C. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    status: "Done",
+  },
+];
+
 const Projects = () => {
   const data = useLoaderData() as UserProfileData;
+  const [projects, setProjects] = useState(mock_projects_data);
   console.log("data", data);
-  const mock_projects_data: Project[] = [
-    {
-      name: "Project A",
-      description:
-        "This is project A. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      status: "To Do",
-    },
-    {
-      name: "Project B",
-      description:
-        "This is project B. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      status: "In Progress",
-    },
-    {
-      name: "Project C",
-      description:
-        "This is project C. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      status: "In Progress",
-    },
-    {
-      name: "Project D",
-      description:
-        "This is project C. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      status: "Testing",
-    },
-    {
-      name: "Project E",
-      description:
-        "This is project C. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      status: "Done",
-    },
-  ];
 
   const sliderSettings = {
     dots: true,
     infinite: true,
-    speed: 400,
-    slidesToShow: 3,
+    speed: 700,
+    slidesToShow: 2,
     slidesToScroll: 1,
+    arrows: true,
   };
 
   return (
@@ -62,7 +66,7 @@ const Projects = () => {
         {data.name}'s Projects
       </Text>
       <Slider {...sliderSettings}>
-        {mock_projects_data.map((project, index) => (
+        {projects.map((project, index) => (
           <Box
             key={index}
             alignContent="center"
@@ -71,16 +75,26 @@ const Projects = () => {
             bg="gray.100"
             border="1px solid black"
           >
-            <Text mb={2}>{project.name}</Text>
-            <Text noOfLines={3}>{project.description}</Text>
-            <Text mt={2} as="b">
-              {project.status}
+            <Text mb={4} as="b">
+              {project.name}
             </Text>
+            <Text noOfLines={1}>{project.description}</Text>
+            <Box>
+              {project.status === "In Progress" ? (
+                <Text color="orange">{project.status}</Text>
+              ) : project.status === "Done" ? (
+                <Text color="green">{project.status}</Text>
+              ) : project.status === "Testing" ? (
+                <Text color="purple">{project.status}</Text>
+              ) : project.status === "To Do" ? (
+                <Text color="red">{project.status}</Text>
+              ) : null}
+            </Box>
           </Box>
         ))}
       </Slider>
       <Box mt={14}>
-        <ProjectAccordion />
+        <ProjectAccordion projects={projects} setProjects={setProjects} />
       </Box>
     </Box>
   );
