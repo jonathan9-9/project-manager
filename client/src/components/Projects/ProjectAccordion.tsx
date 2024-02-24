a9696a64d2034121a9fe6aed7f73a4f2c0d21085
Jonathan <jonathancornejox@gmail.com>
2024-02-24 11:04:14 -0800

    accordion create project section

--- client/src/components/Projects/ProjectAccordion.tsx
+++ client/src/components/Projects/ProjectAccordion.tsx
@@ -4,11 +4,31 @@ import {
   AccordionItem,
   AccordionPanel,
   Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Textarea,
 } from "@chakra-ui/react";
 
 import { AddIcon, CloseIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
 const ProjectAccordion = () => {
+  const [name, setName] = useState("");
+  const [submittedName, setSubmittedName] = useState(false);
+
+  const [description, setDescription] = useState("");
+
+  const onChangeName = (e: React.FormEvent) => {
+    setName((e.target as HTMLInputElement).value);
+  };
+  const onChangeDescription = (e: React.FormEvent) => {
+    setDescription((e.target as HTMLInputElement).value);
+  };
+
+  const isErrorName = name === "" && submittedName;
   return (
     <Accordion allowMultiple>
       <AccordionItem>
@@ -27,10 +47,26 @@ const ProjectAccordion = () => {
               </AccordionButton>
             </h2>
             <AccordionPanel pb={2} flex="1" border="1px solid black" ml={2}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              <FormControl isInvalid={isErrorName} isRequired>
+                <FormLabel>Project Name:</FormLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  value={name}
+                  onChange={onChangeName}
+                />
+                {!isErrorName ? null : (
+                  <FormErrorMessage>Password is required.</FormErrorMessage>
+                )}
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Description: </FormLabel>
+                <Textarea value={description} onChange={onChangeDescription} />
+              </FormControl>
+              <Button w="100%" onClick={() => {}}>
+                Create
+              </Button>
             </AccordionPanel>
           </>
         )}
