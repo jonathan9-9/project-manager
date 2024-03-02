09af2015cb2a9bf757529c5145bc017f9eea43b2
Jonathan <jonathancornejox@gmail.com>
2024-03-02 12:44:11 -0800

    modal overlay

--- client/src/components/Features/FeatureModal.tsx
+++ client/src/components/Features/FeatureModal.tsx
@@ -18,20 +18,14 @@ const FeatureModal = ({ isOpen, onClose }: ModalProps) => {
   return (
     <>
       <Modal onClose={onClose} isOpen={isOpen} isCentered>
-        <ModalOverlay />
+        <ModalOverlay
+          bg="blackAlpha.300"
+          backdropFilter="blur(10px) hue-rotate(90deg)"
+        />
         <ModalContent>
           <ModalHeader>Modal Title</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            Contrary to popular belief, Lorem Ipsum is not simply random text.
-            It has roots in a piece of classical Latin literature from 45 BC,
-            making it over 2000 years old. Richard McClintock, a Latin professor
-            at Hampden-Sydney College in Virginia, looked up one of the more
-            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-            going through the cites of the word in classical literature,
-            discovered the undoubtable source. Lorem Ipsum comes from sections
-            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
-          </ModalBody>
+          <ModalBody>This is the body of the modal</ModalBody>
           <ModalFooter>
             <Button onClick={onClose}>Close</Button>
           </ModalFooter>
