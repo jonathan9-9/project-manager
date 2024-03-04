08e341d80e5a605a1c21f1842c3d9d721a338e51
Jonathan <jonathancornejox@gmail.com>
2024-03-03 20:03:15 -0800

    make user story accordion onChange function and onSubmit

--- client/src/components/UserStories/MakeUserStoryAccordion.tsx
+++ client/src/components/UserStories/MakeUserStoryAccordion.tsx
@@ -12,9 +12,40 @@ import {
   Textarea,
   Button,
   Box,
+  useToast,
 } from "@chakra-ui/react";
 
+import { AddIcon, MinusIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+
 const MakeUserStoryAccordion = () => {
+  const toast = useToast();
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [submittedName, setSubmittedName] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const isErrorName = name === "" && submittedName;
+
+  const onChangeName = (e: React.FormEvent) => {
+    setSubmittedName(false);
+    setName((e.target as HTMLInputElement).value);
+  };
+
+  const onChangeDescription = (e: React.FormEvent) => {
+    setDescription((e.target as HTMLInputElement).value);
+  };
+
+  const onSubmitUserStory = () => {
+    setSubmittedName(true);
+
+    if (name !== "") {
+      setIsOpen(false);
+
+      const token = localStorage.getItem("token");
+    }
+  };
   return (
     <Accordion allowToggle>
       <AccordionItem border="0px">
@@ -34,7 +65,7 @@ const MakeUserStoryAccordion = () => {
             </h2>
 
             <AccordionPanel textColor="white" border="1px solid" pb={4}>
-              <FormControl isInvalid="" isRequired mb={4}>
+              <FormControl isInvalid={isErrorName} isRequired mb={4}>
                 <FormLabel>User Story Name:</FormLabel>
                 <Input type="text" value={name} onChange={onChangeName} />
                 {!isErrorName ? null : (
