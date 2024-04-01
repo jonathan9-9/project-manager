2b838236cdd5e60d38d7c92057d86203f7e9a5fa
Jonathan <jonathancornejox@gmail.com>
2024-03-31 18:22:38 -0700

    project ordering in descending order from latest to oldest project creation

--- api/src/projects/projects.service.ts
+++ api/src/projects/projects.service.ts
@@ -23,6 +23,7 @@ export class ProjectsService {
             },
           },
         },
+        orderBy: { id: 'desc' },
       });
 
       return projects;
--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,5 +1,7 @@
 const Footer = () => {
-  return <div id="footer">&#169; 2023-2024 Unifier | All Rights Reserved</div>;
+  return (
+    <div id="footer">&#169; 2023-2024 UnitedOne | All Rights Reserved</div>
+  );
 };
 
 export default Footer;
